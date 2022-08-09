d87e79b3d138996238361b9f180331e48d5cf949
Deepak Kumar <iamdpk18@gmail.com>
2022-08-10 02:55:07 +0530

    Refactor Div into components

--- src/components/excitingFeatures.jsx
+++ src/components/excitingFeatures.jsx
@@ -6,44 +6,37 @@ import Checkup from "../assets/images/checkup.png";
 import Tests from "../assets/images/tests.png";
 
 import "./excitingFeatures.css";
+import FeaturesComponent from "./featuresComponent";
 
 function ExcitingFeaturesSection() {
   return (
     <div className="container exciting-features-container">
       <h4 className="heading">Meet exciting feature of Health Insurance</h4>
       <div className="row row-cols-4 mt-5 pt-5">
-        <div className="col">
-          <img src={Cashless} alt="Cashless Payments" />
-          <p className="main-txt mt-3">Cashless Treatment</p>
-          <p className="sub-txt">
-            Get your blood tests delivered at home collect a sample from the
-            your blood tests.
-          </p>
-        </div>
-        <div className="col">
-          <img src={Checkup} alt="Medical Checkups" />
-          <p className="main-txt mt-3">Medical CheckUps</p>
-          <p className="sub-txt">
-            Get your blood tests delivered at home collect a sample from the
-            your blood tests.
-          </p>
-        </div>
-        <div className="col">
-          <img src={App} alt="Health App" />
-          <p className="main-txt mt-3"> Dedicated Health App </p>
-          <p className="sub-txt">
-            Get your blood tests delivered at home collect a sample from the
-            your blood tests.
-          </p>
-        </div>
-        <div className="col">
-          <img src={Tests} alt="Reimbursement of tests" />
-          <p className="main-txt mt-3"> Reimbursement of different tests </p>
-          <p className="sub-txt">
-            Get your blood tests delivered at home collect a sample from the
-            your blood tests.
-          </p>
-        </div>
+        <FeaturesComponent
+          image={Cashless}
+          altText="Cashless Payments"
+          heading="Cashless Treatment"
+          body="Get your blood tests delivered at home collect a sample from the your blood tests."
+        />
+        <FeaturesComponent
+          image={Checkup}
+          altText="Medical Checkups"
+          heading="Medical Checkups"
+          body="Get your blood tests delivered at home collect a sample from the your blood tests."
+        />
+        <FeaturesComponent
+          image={App}
+          altText="Health App"
+          heading="Dedicated Health App"
+          body="Get your blood tests delivered at home collect a sample from the your blood tests."
+        />
+        <FeaturesComponent
+          image={Tests}
+          altText="Reimbursement of tests"
+          heading="Reimbursement of different tests"
+          body="Get your blood tests delivered at home collect a sample from the your blood tests."
+        />
       </div>
     </div>
   );
